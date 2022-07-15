fc91c41a9692b173478ebcdea55907a748355623
ArtSuvor <85563543+ArtSuvor@users.noreply.github.com>
2022-07-15 21:00:29 +0300

    Initial Commit

---
+++ StanfordCourseSwiftUI/StanfordCourseSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StanfordCourseSwiftUIApp.swift
+//  StanfordCourseSwiftUI
+//
+//  Created by Art on 15.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct StanfordCourseSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
